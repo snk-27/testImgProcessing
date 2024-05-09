b4d00f5c2e7e61624c6493fd1ab0b4a8eca0d15e
snk-27 <119646221+snk-27@users.noreply.github.com>
2024-05-10 00:53:05 +0800

    Update test.py

--- test.py
+++ test.py
@@ -8,17 +8,20 @@ def main():
     cap = cv2.VideoCapture(0)
     frame_placeholder = st.empty()
     stop_button_pressed = st.button("Stop")
+    frame_displayed = False
     while cap.isOpened() and not stop_button_pressed:
         ret, frame = cap.read()
         if not ret:
             st.write("Video Capture Ended")
             break
+        frame_displayed = True
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        frame_placeholder.image(frame,channels="RGB")
+        frame_placeholder.image(frame, channels="RGB")
         if cv2.waitKey(1) & 0xFF == ord("q") or stop_button_pressed:
             break
     cap.release()
-    cv2.destroyAllWindows()
+    if frame_displayed:
+        cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     main()
