08f40197e0821529bf9e3ecd5a834034fa840f9e
snk-27 <119646221+snk-27@users.noreply.github.com>
2024-05-09 22:24:00 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,12 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 import streamlit as st
 import cv2
+import numpy as np
 
 def takeImage():
-    uploaded_file = st.file_uploader("Choose a image", type = ['jpg', 'png'])
-    
-    if uploaded_file is not None:   
-        img = uploaded_file.getvalue()
+    uploaded_file = st.file_uploader("Choose a image", type=['jpg', 'png'])
+
+    if uploaded_file is not None:
+        img = uploaded_file.read()
         if img:
             st.success("Image Uploaded Successfully")
         return img
@@ -21,13 +19,13 @@ st.divider()
 col1, col2 = st.columns(2)
 
 with col1:
-    inp_img = takeImage() 
-    
+    inp_img = takeImage()
+
     # storing image as a file
     if inp_img:
-        with open("input.jpg","wb") as f:
+        with open("input.jpg", "wb") as f:
             f.write(inp_img)
-        
+
 with col2:
     st.write(
         """
@@ -36,7 +34,7 @@ with col2:
     )
     st.image(inp_img)
 
-image = cv2.imread('./input.jpg')
+image = cv2.imdecode(np.frombuffer(inp_img, np.uint8), cv2.IMREAD_COLOR)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
@@ -49,27 +47,27 @@ faces = faceCascade.detectMultiScale(
     minSize=(15, 15)
 )
 
-for (x,y,w,h) in faces:
-    cv2.rectangle(image,(x,y),(x+w,y+h),(255,0,0),2)
-
-for (x,y,w,h) in faces:
-    roi_gray = gray[y:y+h, x:x+w]
-    roi_color = image[y:y+h, x:x+w]
-    eyes = eye_cascade.detectMultiScale(roi_gray)
-    for (ex,ey,ew,eh) in eyes:
-        cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+for (x, y, w, h) in faces:
+    cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
 for (x, y, w, h) in faces:
-    cv2.rectangle(image, (x, y), (x + w, y + h), (0,0,255), 2)
+    roi_gray = gray[y:y + h, x:x + w]
+    roi_color = image[y:y + h, x:x + w]
+    eyes = eye_cascade.detectMultiScale(roi_gray)
+    for (ex, ey, ew, eh) in eyes:
+        cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
 
-status = cv2.imwrite('output.jpg', image)
+for (x, y, w, h) in faces:
+    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+status = cv2.imencode('.jpg', image)[1].tobytes()
 print("[INFO] Image besiktas2.jpg written to filesystem: ", status)
 
 st.divider()
 
 if len(faces) != 0:
     st.write(f"""### Output""")
-    st.image('./output.jpg')
+    st.image(image, channels="BGR")
     st.write(f"""#### Information:""")
     if len(faces) > 1:
         st.write("Found {0} Faces in the image!".format(len(faces)))
