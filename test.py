5820bf1678ea90c29aa6f2fea84f430b18428975
snk-27 <119646221+snk-27@users.noreply.github.com>
2024-05-10 00:01:25 +0800

    Update test.py

--- test.py
+++ test.py
@@ -20,5 +20,10 @@ while cap.isOpened() and not stop_button_pressed:
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     frame_placeholder.image(frame, channels="RGB")
 
+    if cv2.waitKey(1) & 0xFF == ord("q") or stop_button_pressed:
+        break
+
+if cv2.getWindowProperty("frame", cv2.WND_PROP_VISIBLE):
+    cv2.destroyAllWindows()
+
 cap.release()
-cv2.destroyAllWindows()
