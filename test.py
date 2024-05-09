637a74489e0b9da3bf3e31e85b62c71e46e18d8a
snk-27 <119646221+snk-27@users.noreply.github.com>
2024-05-10 01:12:05 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,16 @@
-import streamlit as st
 from streamlit_webrtc import webrtc_streamer
+import av
+import cv2
 
-webrtc_streamer(key="example")
+cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+class VideoProcessor:
+  def recv(self, frame):
+      frm = frame.to_ndarray(format="bgr24")
+      faces = cascade.detectMultiScale(cv2.cvtColor(frm, cv2.COLOR_BGR2GRAY), 1.1, 3)
+      for x,y,w,h in faces:
+        cv2.rectangle(frm, (x,y), (x+w, y+h), (0,255,0), 3)
+      
+      return av.VideoFrame.from_ndarray(frm, format='bgr24')
+    
+webrtc_streamer(key="key", video_processor_factor=VideoProcessor)
